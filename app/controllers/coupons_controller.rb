336341365b5f6474f2113746f0d480bac1f254d5
Alex Zdatny <alexzdatny@gmail.com>
2018-09-09 00:15:51 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,4 +15,6 @@ class CouponsController < ApplicationController
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
+    @coupon.save
+    redirect to coupon_path(@coupon)
 end
